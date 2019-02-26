a66efcc033863901d29a6dd98b8b363fbbc9961b
pilamb <perikopalotes@gmail.com>
2019-02-26 18:40:40 +0100

    serializing pains on user detail view

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,17 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+from data_api.models import Hit
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-     class Meta:
-         model = User
-         fields = ('url', 'username', 'email', 'groups')
-
+    pains = serializers.PrimaryKeyRelatedField(many=True,
+                                               queryset=Hit.objects.all())
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups', 'pains')
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
         class Meta:
             model = Group
             fields = ('url', 'name')
-
